bff7548a3898370a9d51d754ce701078591005b3
Stefan Raspopescu <stefan.r95@hotmail.com>
2022-05-25 19:02:07 +0200

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,5 +1,5 @@
 <?php
-echo "Stefan Razvan Raspopescu Marcu";
+// Autor: Stefan Razvan Raspopescu Marcu <stefan.r95@hotmail.com>
 // El nombre por defecto es Mundo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";
