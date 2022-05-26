899e6516ff0109421c1139af60a2ef16f70b852c
Stefan Raspopescu <stefan.r95@hotmail.com>
2022-05-26 12:40:25 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php
 // Autor: Stefan Razvan Raspopescu Marcu <stefan.r95@hotmail.com>
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
